676ba582b11c92e4b888ee3e024442f383821420
levuphucnguyen <levuphucnguyen@gmail.com>
2023-07-01 10:06:11 +0700

    use func

--- songuyento.cpp
+++ songuyento.cpp
@@ -3,19 +3,21 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
+
+bool prime(long long n) {
+    for (long long i = 2; i <= sqrt(n); i++){
+        if (n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
 int main(){
     freopen("songuyento.inp", "r", stdin);
     freopen("songuyento.out", "w", stdout);
-    int n;
+    long long n;
     cin >> n;
-    bool yn = true;
-    for (int i = 2; i <= sqrt(n); i++){
-        if (n%i == 0){
-            yn = false;
-            break;
-        }
-    }
-    if (yn==true) cout << "YES";
-    if (yn == false) cout << "NO";
+    if (prime(n)) cout << "YES";
+    else cout << "NO";
     return 0;
 }
