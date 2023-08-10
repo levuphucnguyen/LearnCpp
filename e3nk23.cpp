4f066ae36542399fd01cb87db153333860b4b2d5
levuphucnguyen <96976083+levuphucnguyen@users.noreply.github.com>
2023-08-10 15:28:07 +0700

    finish e3nk23.cpp

--- e3nk23.cpp
+++ e3nk23.cpp
@@ -7,46 +7,48 @@
 
 using namespace std;
 
-int sum(int a[], int s, int e){
-    int res = 0;
-    while (s <= e){
-        res += a[s];
-        s++;
+int countxinvector(int x; vector<int> v) {
+    int cnt;
+    for (i : v) {
+        if (i == x) cnt++;
     }
-    return res ;
+    return cnt;
 }
+
 
 int main() {
     ifstream cin("e3nk23.inp");
     ofstream cout("e3nk23.out");
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n, k;
+    int n;
     cin >> n;
     int a[n];
-    for (int &x : a) cin >> x;
+    for (int x : a) cin >> x;
+    int k;
     cin >> k;
-    set<int> uk;
-    set<int> dr;
-    for (int i = 1; i <= sqrt(k); i++) {
+    // find all divisors of k
+    vector<pair<int, int>> divk;
+    //divk[].first is the smaller divisor
+    for (i = 1; i <= sqrt(k); i++) {
         if (k % i == 0) {
-            uk.insert(i);
+            divk.push_back(make_pair(i, k/i))
         }
     }
-    for (int s = 0; s <= n - 1; s++) {
-        for (int e = n-1; e >= s; e--) {
-            if (uk.count(sum(a, s, e)) != 0) {
-                dr.insert(k/(sum(a, s, e)));
-            }
+    int pref[n+1]; pref[0] = 0
+    for (int i = 1, i <= n; i++) {
+        pref[i] = pref[i-1] + a[i-1];
+    }
+    vector<int> subsum
+    for (int beg = 0; beg < n; beg++) {
+        for (int end = 0; end < n; end++) {
+            subsum.push_back(pref[end+1] - pref[beg]);
         }
     }
-    for (int s = 0; s <= n - 1; s++) {
-        for (int e = n-1; e >= s; e--) {
-            if (dr.count(sum(a, s, e)) == 0) {
-                dr.erase(sum(a, s, e));
-            }
-        }
+    int res = 0;
+    for (pair<int, int> div : divk) {
+        res += (countxinvector(div.first, subsum) * countxinvector(div.second, subsum))
     }
-    cout << dr.size();
+    cout << res;
     return 0;
 }
