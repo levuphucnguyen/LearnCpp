e6dd8a18cbbb66302991b592bb2139ea8fb35f3f
unknown <levuphucnguyen@gmail.com>
2023-08-12 16:50:13 +0700

    optimize e2nk23

---
+++ e2nk23/main.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+int main(){
+    ifstream cin("e2nk23.inp");
+    ofstream cout("e2nk23.out");
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int M, N, K;
+    cin >> M >> N >> K;
+    int inM[4*K], inN[4*K];
+    for (int i = 0; i < 4*K; ++i) {
+        cin >> inM[i] >> inN[i];
+    }
+    map<int, int> posM, posN;
+    for (int i : inM) {
+        ++posM[i];
+    }
+    for (int i: inN) {
+        ++posN[i];
+    }
+    int cntM = 0, cntN = 0, blankM, blankN;
+    for (map<int, int>::iterator it = posM.begin(); cntM < 2*K; ++it) {
+        cntM += (*it).second;
+        if (cntM == 2*K) {
+            blankM = (*(it++)).first - (*it).first - 1;
+        }
+    }
+    for (map<int, int>::iterator it = posN.begin(); cntN < 2*K; ++it) {
+        cntN += (*it).second;
+        if (cntN == 2*K) {
+            blankN = (*(it++)).first - (*it).first - 1;
+        }
+    }
+    cout << (blankM + 1) * (blankN + 1);
+    return 0;
+}
